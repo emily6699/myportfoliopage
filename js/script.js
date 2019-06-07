28192a21f78c4170761a3576530fd165e162b5ff
emily <emilylu369@gmail.com>
2019-06-06 21:48:34 -0400

    made software engineer/web developer typed in

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
     play: 5000,
     pagination: false
   });
+
+  var typed = new Typed(".typed", {
+    strings: ["Software Engineer.", "Web Developer."],
+    typeSpeed: 70,
+    loop: true,
+    strtDelay: 1000,
+    showCursor: false
+  });
 });
