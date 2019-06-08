a42bea4947447fd47e4869cbee5be831b834375a
emily <emilylu369@gmail.com>
2019-06-07 23:14:55 -0400

    styled icons and crop images

--- js/script.js
+++ js/script.js
@@ -57,5 +57,15 @@ $(document).ready(function() {
       });
     }
   });
+
   $("[data-fancybox").fancybox();
+
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false
+    }
+  });
 });
